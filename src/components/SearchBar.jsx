a883afe8cd1f3f5c66b9e52ed56a27317e7d26f7
Kelly Ton <kthton@gmail.com>
2025-04-17 16:56:44 -0400

    Style search results bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -72,6 +72,45 @@ function SearchBar({ onLocationSelect }) {
                 onChange={(event, newValue) => setSelectedLocation(newValue)}
                 inputValue={searchInput}
                 onInputChange={(event, newInputValue) => setSearchInput(newInputValue)}
+                renderOption={(props, option) => (
+                    <Box
+                        component="li"
+                        {...props}
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'flex-start',
+                            padding: '8px 12px',
+                            backgroundColor: '#fff6e5', // soft warm tan
+                            borderBottom: '1px solid #e0c896',
+                            '&:hover': {
+                                backgroundColor: '#f3ddb3',
+                            }
+                        }}
+                    >
+                        <span style={{ fontWeight: 600 }}>{option.name}</span>
+                        <span style={{ fontSize: 12, color: '#555' }}>
+                            {option.state ? `${option.state}, ` : ""}{option.country}
+                        </span>
+                    </Box>
+                )}
+                componentsProps={{
+                    popper: {
+                        sx: {
+                            borderRadius: 2,
+                            boxShadow: 3,
+                            mt: 1,
+                            backgroundColor: '#fff6e5', // same soft tan as list items
+                            color: 'black',
+                            zIndex: 1300,
+                        }
+                    },
+                    paper: {
+                        sx: {
+                            backgroundColor: '#fff6e5', // match with your theme
+                        }
+                    }
+                }}
                 renderInput={(params) => (
                     <TextField
                         {...params}
